5881c3578b604d34e30e841edbe7f0113eb9ee9d
Pietro Martinelli <amicofragile@gmail.com>
2015-03-07 15:28:35 +0100

    Refactored tests extracting 'OWNER' constant

--- src/test/java/it/unibs/eng/fp/bank/BankAccountTest.java
+++ src/test/java/it/unibs/eng/fp/bank/BankAccountTest.java
@@ -5,27 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BankAccountTest {
+	private static final String OWNER = "Pietro Martinelli";
+
 	@Test
 	public void initializeOwnerAndBalance() throws Exception {
-		final String owner = "Pietro Martinelli";
 		final Long balance = 2000L;
-		final BankAccount account = new BankAccount(owner, balance);
-		assertAccount(account, owner, balance);
+		final BankAccount account = new BankAccount(OWNER, balance);
+		assertAccount(account, OWNER, balance);
 	}
 
 	@Test
 	public void initializeUsingDefaultInitialBalance() throws Exception {
-		final String owner = "Pietro Martinelli";
-		final BankAccount account = new BankAccount(owner);
-		assertAccount(account, owner, 0L);
+		final BankAccount account = new BankAccount(OWNER);
+		assertAccount(account, OWNER, 0L);
 	}
 
 	@Test
 	public void depositABitOfMoney() throws Exception {
-		final String owner = "Pietro Martinelli";
-		final BankAccount account = new BankAccount(owner, 1500L);
+		final BankAccount account = new BankAccount(OWNER, 1500L);
 		account.deposit(1000L);
-		assertAccount(account, owner, 2500L);
+		assertAccount(account, OWNER, 2500L);
 	}
 
 	private void assertAccount(BankAccount account, String owner, Long balance) {
