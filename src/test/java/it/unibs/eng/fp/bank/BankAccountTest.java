ecbd441ddf72762be5b8d2ab93d52d6fc753753d
Pietro Martinelli <amicofragile@gmail.com>
2015-03-07 15:24:10 +0100

    Refactor tests, extracting 'assertAccount' method

--- src/test/java/it/unibs/eng/fp/bank/BankAccountTest.java
+++ src/test/java/it/unibs/eng/fp/bank/BankAccountTest.java
@@ -10,15 +10,18 @@ public class BankAccountTest {
 		final String owner = "Pietro Martinelli";
 		final Long balance = 2000L;
 		final BankAccount account = new BankAccount(owner, balance);
-		assertEquals(owner, account.getOwner());
-		assertEquals(balance, account.getBalance());
+		assertAccount(account, owner, balance);
 	}
 
 	@Test
 	public void initializeUsingDefaultInitialBalance() throws Exception {
 		final String owner = "Pietro Martinelli";
 		final BankAccount account = new BankAccount(owner);
+		assertAccount(account, owner, 0L);
+	}
+
+	private void assertAccount(BankAccount account, String owner, Long balance) {
 		assertEquals(owner, account.getOwner());
-		assertEquals(Long.valueOf(0L), account.getBalance());
+		assertEquals(balance, account.getBalance());
 	}
 }
